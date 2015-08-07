bb648aeea9ed1ff45e2116e7b22577dd87322d1c
Andreas Copan <avcopan@gmail.com>
2015-08-07 00:49:51 -0400

    Changes

--- examples/plot_eom-ee_qchem.py
+++ examples/plot_eom-ee_qchem.py
@@ -6,8 +6,12 @@ print singlets.dataseries
 #print singlets.keys
 #print singlets.labels
 
+import matplotlib
 import matplotlib.pyplot as pp
 
+matplotlib.rc('font', family='sans-serif') 
+matplotlib.rc('font', serif='Helvetica') 
+matplotlib.rc('text', usetex='false') 
 singlets.plot_all('b-')
 pp.savefig('singlets.pdf')
 
